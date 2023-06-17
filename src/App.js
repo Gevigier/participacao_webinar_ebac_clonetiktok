f6dd56bb6bea168993936e55c43bcd67726aff8b
Marcos G <117698420+Gevigier@users.noreply.github.com>
2023-06-16 22:49:24 -0300

    aula_04
    Participação da Aula 04 de tema:
    "Componente Sidebar e preparação do UI para receber DB"

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ function App() {
   return (
     <div className="App">
       <div className="app__videos">
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video
+          likes = {458}
+          messages = {264}
+          share = {327}
+          name = "Marcos GG"
+          description = "Gato + Goleiro = Gatoleiro"
+          music = "Epic Music"
+          url = "https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+          />
+        <Video
+          likes = {678}
+          messages = {179}
+          share = {891}
+          name = "EBAC Online"
+          description = "Video provido 02"
+          music = "Clap your hands"
+          url = "https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z"
+        />
       </div>
     </div>
   );
